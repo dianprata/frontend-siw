9b6322c82c75f83d17d7bf90ab88a55c36b76dbc
Dian <dianpratama2@gmail.com>
2020-09-10 19:49:35 +0800

    dist

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,8 @@
-const purgecss = require("@fullhuman/postcss-purgecss");
+// const purgecss = require("@fullhuman/postcss-purgecss");
 module.exports= {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
     require('postcss-rtl'),
-    //Only add purgecss in production
-    process.env.NODE_ENV === "production"? purgecss({
-      content: [
-        "./src/**/*.html",
-        "./src/**/*.vue"
-      ]
-    }): ""
   ]
 }
