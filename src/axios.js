3819fcf42c25b35b5f23e50bf8e5cfa33fece7fb
Muhammad Fuad Ardiono <fuad@investree.id>
2020-09-20 05:48:46 +0800

    Complaint export csv excel #FUAD JAGO BANGET

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,7 @@
 import axios from 'axios'
 import state from './store/state'
 
-const baseURL = "https://api.sip-poltek.xyz";
-// const baseURL = "http://localhost:3000";
+const baseURL = process.env.VUE_APP_API_BASEURL;
 
 axios.defaults.headers.common['Authorization'] = `${state.AppActiveUser.token ? state.AppActiveUser.token : ''}`;
 
